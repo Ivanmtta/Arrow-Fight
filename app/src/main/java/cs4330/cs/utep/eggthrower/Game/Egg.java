a449ac754e572bb1ead113cf66c9f3444820046b
Ivan Mota <imotaaguil@miners.utep.edu>
2019-04-24 14:45:03 -0600

    Added Egg Exploding mechanic

--- app/src/main/java/cs4330/cs/utep/eggthrower/Game/Egg.java
+++ app/src/main/java/cs4330/cs/utep/eggthrower/Game/Egg.java
@@ -1,7 +1,11 @@
 package cs4330.cs.utep.eggthrower.Game;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
+
+import cs4330.cs.utep.eggthrower.R;
 
 public class Egg {
 
@@ -12,6 +16,8 @@ public class Egg {
     private Bitmap sprite;
     public boolean moving;
     public boolean inAir;
+    private boolean exploding;
+    private Animation explodeAnimation;
 
     public Egg(int x, int y, int width, int height, Bitmap sprite){
         position = new Vector2(x, y);
@@ -19,6 +25,13 @@ public class Egg {
         this.width = width;
         this.height = height;
         this.sprite = Bitmap.createScaledBitmap(sprite, width, height, true);
+        initializeAnimation();
+    }
+
+    public void initializeAnimation(){
+        Bitmap[] frames = new Bitmap[4];
+        frames[0] = BitmapFactory.decodeResource(GameView.resources, R.drawable.basket);
+        explodeAnimation = new Animation(frames, .25f);
     }
 
     public void update(){
@@ -27,11 +40,31 @@ public class Egg {
             velocity.setY(velocity.getY() + 0.35f);
             velocity.setX(velocity.getX() * 0.99f);
             velocity.setY(velocity.getY() * 0.99f);
+            if(!exploding) {
+                for (Egg currentEgg : GameView.opponentEggs) {
+                    if (this.contains(currentEgg)) {
+                        explode();
+                    }
+                }
+            }
+            else{
+                explodeAnimation.play();
+            }
         }
     }
 
     public void render(Canvas canvas){
-        canvas.drawBitmap(sprite, position.getX(), position.getY(), null);
+        if(exploding){
+            canvas.drawBitmap(explodeAnimation.getCurrentFrame(), position.getX(), position.getY(), null);
+        }
+        else{
+            canvas.drawBitmap(sprite, position.getX(), position.getY(), null);
+        }
+    }
+
+    public void explode(){
+        exploding = true;
+        velocity.set(0f, 0f);
     }
 
     public void launch(double angle, double force){
@@ -43,5 +76,15 @@ public class Egg {
         int extra = (int)(50f / GameView.SCALE_RATIO);
         return (position.getX() - extra <= vector.getX()) && (vector.getX() < position.getX() + width + extra) &&
                 ((position.getY() - extra <= vector.getY()) && (position.getY() < position.getY() + height + extra));
+    }
+
+    public boolean contains(Egg egg){
+        Rect eggHitBox = new Rect(
+                (int) egg.position.getX(),
+                (int) egg.position.getY(),
+                (int) (egg.position.getX() + egg.width),
+                (int) (egg.position.getY() + egg.height));
+        Rect hitBox = new Rect((int)position.getX(), (int)position.getY(), (int)(position.getX() + width), (int)(position.getY() + height));
+        return hitBox.contains(eggHitBox);
     }
 }
